098a12fbc403970da8588651626b8c68d617e503
Nobuhiro IMAI <nov@yo.rim.or.jp>
2009-03-19 12:48:04 +0900

    fix functional test

--- test/functional/archive_controller_test.rb
+++ test/functional/archive_controller_test.rb
@@ -1,6 +1,9 @@
 require File.dirname(__FILE__) + '/../test_helper'
 require "archive_controller"
 
+# Re-raise errors caught by the controller.
+class ArchiveController; def rescue_action(e) raise e end; end
+
 class ArchiveControllerTest < ActionController::TestCase
   include ArchiveHelper
   fixtures :articles, :users
@@ -18,6 +21,16 @@ class ArchiveControllerTest < ActionController::TestCase
       :controller => "archive", :action => "show", :id => "bar", :part => "2")
   end
 
+  def test_to_html
+    with_settings(:plugin_redmine_archive => {"archive_key" => ""}) do
+      assert_equal "<p>[test:1234]</p>", Redmine::WikiFormatting.to_html(Setting.text_formatting, "[test:1234]")
+    end
+    with_settings(:plugin_redmine_archive => {"archive_key" => "hogefuga-foo-bar"}) do
+      assert_equal('<p><a href="/archive/show/bQKq_vjzFjz7MBFxi7CdOw==">[test:1234]</a></p>',
+        Redmine::WikiFormatting.to_html(Setting.text_formatting, "[test:1234]"))
+    end
+  end
+
   def test_not_logged_in
     @request.session[:user_id] = nil
     get :show, :id => "foo"
@@ -27,7 +40,7 @@ class ArchiveControllerTest < ActionController::TestCase
 
   def test_show
     @request.session[:user_id] = 1
-    with_settings(:mail_handler_api_key => "hogefuga-foo-bar") do
+    with_settings(:plugin_redmine_archive => {"archive_key" => "hogefuga-foo-bar"}) do
       params = archive("[test:1]")
       get params[:action], :id => params[:id]
       assert_response :success
@@ -38,7 +51,7 @@ class ArchiveControllerTest < ActionController::TestCase
 
   def test_show_coverpage
     @request.session[:user_id] = 1
-    with_settings(:mail_handler_api_key => "hogefuga-foo-bar") do
+    with_settings(:plugin_redmine_archive => {"archive_key" => "hogefuga-foo-bar"}) do
       params = archive("[test:2]")
       get params[:action], :id => params[:id]
       assert_response :success
@@ -46,13 +59,14 @@ class ArchiveControllerTest < ActionController::TestCase
       assert_template "show"
       assert_tag(:tag => "ul", :attributes => {:class => "properties"},
         :child => {:tag => "li",
-        :descendant => {:tag => "a", :content => /sample.patch/}})
+          :descendant => {:tag => "a", :content => /sample.patch/,
+            :attributes => {:href => "/archive/show/#{params[:id]}/1"}}})
     end
   end
 
   def test_show_part
     @request.session[:user_id] = 1
-    with_settings(:mail_handler_api_key => "hogefuga-foo-bar") do
+    with_settings(:plugin_redmine_archive => {"archive_key" => "hogefuga-foo-bar"}) do
       params = archive("[test:2]")
       get params[:action], :id => params[:id], :part => 1
       assert_response :success
